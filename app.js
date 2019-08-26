cfb6eca26135002e754f061458eb2847f703f65f
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2019-08-26 15:39:05 +0200

    generic

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { Server } = require('@hapi/hapi');
 const Inert = require('@hapi/inert');
 
 const server = new Server({
-    port: 3001,
+    port: 3000,
     routes: {
         files: {
             relativeTo: path.join(__dirname, 'public') //this one makes html and js files available
--- public/index.js
+++ public/index.js
@@ -1,1 +1,39 @@
-console.log('Logging from index.js');
+const keycloak = Keycloak();
+let authorization; // TODO see if this is needed for anything
+
+keycloak.onTokenExpired = function () {
+    console.log('calling refresh...');
+    keycloak.updateToken().success(function(refreshed) {
+        if (refreshed) {
+            console.log('Token refreshed');
+        } else {
+            console.log('Token not refreshed.... no idea why lol');
+        }
+    }).error(function() {
+        console.log('Error refreshing token')
+    });
+};
+
+keycloak.init({
+    responseMode: 'fragment',
+    flow: 'standard',
+    onload: 'check-sso'
+}).success(function(authenticated) {
+    authorization = new KeycloakAuthorization(keycloak);
+});
+
+const displayDiv = document.getElementById('display-div');
+
+function getResource(name) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            displayDiv.innerHTML = this.responseText;
+        } else {
+            displayDiv.innerHTML = 'You may not access this resource! FOOL!';
+        }
+    };
+    xhttp.open("GET", "http://localhost:3001/private/" + name, true);
+    xhttp.setRequestHeader('Authorization', 'Bearer ' + keycloak.token)
+    xhttp.send();
+}
